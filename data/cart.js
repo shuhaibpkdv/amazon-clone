a08f17f8b473369d66ea83cef84cc5eed8bbe3fb
muhammed-shuhaib <muhammedshuhaibpkdv@gmail.com>
2025-01-21 17:29:36 +0530

    In checkout page created proct carts and maked delete button interactive

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,13 @@
-export const cart = [];
+export let cart = [{
+    productId : 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity : 2,
+},{
+    productId : '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity : 1,
+},{
+    productId : '3ebe75dc-64d2-4137-8860-1f5a963e534b',
+    quantity : 1,
+}];
 
 export function addToCart(productId) {
     let matchingItem;
@@ -17,4 +26,16 @@ export function addToCart(productId) {
             quantity : 1
         }); 
     }
+}
+
+export function removeFromCart(productId) {
+    const newCart = [];
+
+    cart.forEach((cartItem) => {
+        if(cartItem.productId !== productId) {
+            newCart.push(cartItem);
+        }
+    });
+
+    cart = newCart;
 }
